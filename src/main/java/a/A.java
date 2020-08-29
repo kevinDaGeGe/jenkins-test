0e5b6bf0ecb1ee8337a7b8d02a6db93a9abbfcbe
Administrator <Administrator@kevin>
2020-08-29 10:10:53 +0800

    1

---
+++ src/main/java/a/A.java
@@ -0,0 +1,12 @@
+package a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(A.class, args);
+	}
+}
